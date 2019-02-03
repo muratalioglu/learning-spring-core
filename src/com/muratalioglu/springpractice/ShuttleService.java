c2289cffb6d24d9a5bc5d30d42cb4b8c46ba0666
= <mdenizalioglu@gmail.com>
2019-02-03 15:29:31 +0300

    add PostConstruct annotation

--- src/com/muratalioglu/springpractice/ShuttleService.java
+++ src/com/muratalioglu/springpractice/ShuttleService.java
@@ -1,8 +1,34 @@
 package com.muratalioglu.springpractice;
 
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class ShuttleService {
+
+    private List<String> shuttleHours = new ArrayList<>();
+
+    @PostConstruct
+    private void readShuttleHourseFromFile() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File("shuttlehours.txt")))) {
+            String hour = null;
+            while ((hour = reader.readLine()) != null) {
+                shuttleHours.add(hour);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public String run() {
-        return "Shuttle Service is running.";
+        return String.format("Shuttle Service is running: " + shuttleHours);
     }
 
     public void initService() {
